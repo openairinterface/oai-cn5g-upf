f63af096de6ccaa0ba3d5e3f73580620652e1b8a
Franck MESSAOUDI <frank.messaoudi@openairinterface.org>
2023-05-25 12:45:52 +0000

    initializing struct headers ETH and IP

--- src/upf_app/bpf/session_bpf.c
+++ src/upf_app/bpf/session_bpf.c
@@ -67,6 +67,9 @@ static u32 create_outer_header_gtpu_ipv4(struct xdp_md *p_ctx, pfcp_far_t_ *p_fa
   bpf_debug("create_outer_header_gtpu_ipv4");
   struct ethhdr *p_eth;
   struct iphdr *p_ip;
+  __builtin_memset(&p_eth, 0, sizeof(struct ethhdr));
+  __builtin_memset(&p_ip, 0, sizeof(struct iphdr));
+  
   void *p_data = (void *)(long)p_ctx->data;
   void *p_data_end = (void *)(long)p_ctx->data_end;
   void *p_mac_address;
