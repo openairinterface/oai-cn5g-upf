1f20742a459a7da2e4dd52d3d170d1ff78269357
Rohan <rohan.kharade@openairinterface.org>
2023-09-04 09:41:12 +0200

    Remove reverse path filter sysctl settings

--- src/upf_app/simpleswitch/pfcp_switch.cpp
+++ src/upf_app/simpleswitch/pfcp_switch.cpp
@@ -344,18 +344,21 @@ void pfcp_switch::setup_pdn_interfaces() {
           "ip -6 addr add {}/{} dev tun{}", conv::toString(addr6).c_str(),
           it.prefix_ipv6, index);
       rc = system((const char*) cmd.c_str());
-      // if ((it.enable_snat) && (/* SGI has IPv6 address*/)){
-      //    cmd = fmt::format("ip6tables -t nat -A POSTROUTING -s {}/{} -o {} -j
-      //    SNAT --to-source {}", conv::toString(addr6).c_str(), it.prefix_ipv6,
-      //    xxx); rc = system ((const char*)cmd.c_str());
-      //}
+      if ((it.enable_snat) && (/* SGI has IPv6 address*/)) {
+        cmd = fmt::format(
+            "ip6tables -t nat -A POSTROUTING -s {}/{} -o {} -j
+            SNAT-- to -
+            source {} ", conv::toString(addr6).c_str(), it.prefix_ipv6,
+            xxx);
+        rc = system((const char*) cmd.c_str());
+      }
     }
     // even if we do nat, we can receive ue ip destinated IP packet
     // but do not forget to set routes outside UPF
-    cmd = fmt::format(
-        "/sbin/sysctl -w net.ipv4.conf.{}.rp_filter=0",
-        upf_cfg.n6.if_name.c_str());
-    rc = system((const char*) cmd.c_str());
+    // cmd = fmt::format(
+    //    "/sbin/sysctl -w net.ipv4.conf.{}.rp_filter=0",
+    //    upf_cfg.n6.if_name.c_str());
+    // rc = system((const char*) cmd.c_str());
 
     // Otherwise redirect incoming ingress UE IP to default gw
     // cmd = fmt::format("/sbin/sysctl -w net.ipv4.conf.tun{}.send_redirects=0",
