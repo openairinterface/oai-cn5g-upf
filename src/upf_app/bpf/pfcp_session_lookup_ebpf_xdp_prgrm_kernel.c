7a7846449d69f146ae9d81739c893621db8d0658
Franck Messaoudi <franck.messaoudi@eurecom.fr>
2024-08-01 13:35:07 +0000

    Update: add a debug log to distinguish PFCP PDR section from FAR section

--- src/upf_app/bpf/pfcp_session_lookup_ebpf_xdp_prgrm_kernel.c
+++ src/upf_app/bpf/pfcp_session_lookup_ebpf_xdp_prgrm_kernel.c
@@ -226,6 +226,7 @@ static __always_inline u32 eth_handle(struct xdp_md* ctx, struct ethhdr* ethh) {
 /*****************************************************************************************************************/
 SEC("xdp_entry_point")
 int entry_point(struct xdp_md* ctx) {
+  bpf_debug("================< PFCP PDR Sesction >================");
   struct ethhdr* ethh = (void*) (long) ctx->data;
 
   if ((void*) (ethh + 1) > (void*) (long) ctx->data_end) {
