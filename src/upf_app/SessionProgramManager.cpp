9bafd18b7fb09b1b69fed74e086cfae80ee1a266
Franck Messaoudi <franck.messaoudi@eurecom.fr>
2024-08-05 08:21:27 +0000

    Revert "Revert "Update: change NextHopFinder as static""
    This reverts commit 9e65b0ea310c7a939afb9adef02401c2e9822d6c.

--- src/upf_app/SessionProgramManager.cpp
+++ src/upf_app/SessionProgramManager.cpp
@@ -81,11 +81,10 @@ void SessionProgramManager::addFarProgram(
 
 /*****************************************************************************************************************/
 uint32_t SessionProgramManager::getRemoteIP(uint32_t upfIP, uint32_t remoteIP) {
-  NextHopFinder finder;
   uint32_t ipnexthop = 0;
-  if (not finder.sameSubnet(upfIP, remoteIP)) {
+  if (not NextHopFinder::sameSubnet(upfIP, remoteIP)) {
     Logger::upf_app().debug("Not in the same subnet");
-    ipnexthop = finder.retrieveNextHopIP(remoteIP);
+    ipnexthop = NextHopFinder::retrieveNextHopIP(remoteIP);
   } else {
     Logger::upf_app().debug("The same subnet");
     ipnexthop = remoteIP;
@@ -217,12 +216,11 @@ void SessionProgramManager::storeFARInFARMap(
 void SessionProgramManager::updateARPTableForN6(
     std::shared_ptr<FARProgram> pFARProgram, uint32_t dnIP, uint32_t upfn6IP) {
   try {
-    NextHopFinder finder;
     // uint32_t remoteN6 = getRemoteIP(upfn6IP, dnIP);
     uint32_t ipnexremoteN6hop = (is_little_endian()) ?
                                     htole32(getRemoteIP(upfn6IP, dnIP)) :
                                     getRemoteIP(upfn6IP, dnIP);
-    auto remoteN6MAC          = finder.retrieveNextHopMAC(ipnexremoteN6hop);
+    auto remoteN6MAC = NextHopFinder::retrieveNextHopMAC(ipnexremoteN6hop);
 
     struct s_arp_mapping map_table;
     memset(&map_table, 0, sizeof(struct s_arp_mapping));
@@ -242,14 +240,12 @@ void SessionProgramManager::updateARPTableForN3(
     std::shared_ptr<FARProgram> pFARProgram, uint32_t gNodeBIP,
     uint32_t upfn3IP, uint32_t seid) {
   try {
-    NextHopFinder finder;
-
     // uint32_t remoteN3 = getRemoteIP(upfn3IP, gNodeBIP);
 
     uint32_t ipnexremoteN3hop = (is_little_endian()) ?
                                     htole32(getRemoteIP(upfn3IP, gNodeBIP)) :
                                     getRemoteIP(upfn3IP, gNodeBIP);
-    auto remoteN3MAC          = finder.retrieveNextHopMAC(ipnexremoteN3hop);
+    auto remoteN3MAC = NextHopFinder::retrieveNextHopMAC(ipnexremoteN3hop);
 
     struct s_arp_mapping map_table;
     memset(&map_table, 0, sizeof(struct s_arp_mapping));
@@ -439,10 +435,9 @@ void SessionProgramManager::createPipeline(
 //       std::thread arpUpdateThread1([this, pFARProgram, seid, gNodeBIP, dnIP,
 //                                     upfn3IP, upfn6IP]() {
 //         try {
-//           NextHopFinder finder;
 
 //           uint32_t remoteN6 = getRemoteIP(upfn6IP, dnIP);
-//           auto remoteN6MAC  = finder.retrieveNextHopMAC(remoteN6);
+//           auto remoteN6MAC  = NextHopFinder::retrieveNextHopMAC(remoteN6);
 
 //           uint32_t ipnexremoteN6hop =
 //               (is_little_endian()) ? htole32(remoteN6) : remoteN6;
@@ -450,7 +445,7 @@ void SessionProgramManager::createPipeline(
 //               ipnexremoteN6hop, remoteN6MAC->ether_addr_octet, BPF_ANY);
 
 //           uint32_t remoteN3 = getRemoteIP(upfn3IP, gNodeBIP);
-//           auto remoteN3MAC  = finder.retrieveNextHopMAC(remoteN3);
+//           auto remoteN3MAC  = NextHopFinder::retrieveNextHopMAC(remoteN3);
 
 //           uint32_t ipnexremoteN3hop =
 //               (is_little_endian()) ? htole32(remoteN3) : remoteN3;
@@ -487,9 +482,8 @@ void SessionProgramManager::createPipeline(
 //       std::thread arpUpdateThread2([this, pFARProgram, dnIP, upfn6IP]() {
 //         try {
 //           // updateArpTableMap(pFARProgram, upfn6IP, ipnexthop);
-//           NextHopFinder finder;
 //           uint32_t remoteN6 = getRemoteIP(upfn6IP, dnIP);
-//           auto remoteN6MAC  = finder.retrieveNextHopMAC(remoteN6);
+//           auto remoteN6MAC  = NextHopFinder::retrieveNextHopMAC(remoteN6);
 
 //           uint32_t ipnexremoteN6hop =
 //               (is_little_endian()) ? htole32(remoteN6) : remoteN6;
