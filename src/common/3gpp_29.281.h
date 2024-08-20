4bd91f11c38b76bce1d23a312faf1437831db866
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2024-08-20 15:32:04 +0200

    Move UPF utils to common src

--- src/common/3gpp_29.281.h
+++ src/common/3gpp_29.281.h
@@ -32,7 +32,6 @@
 #include "3gpp_29.274.h"
 #include "3gpp_commons.h"
 #include "common_root_types.h"
-//#include "logger.hpp"  // for fmt::format in spdlog
 
 #include <arpa/inet.h>
 #include <stdint.h>
