fccbeaa245ee5a97ab8b950c9bd5cd00813671fb
Lukas Rotheneder <lukas.rotheneder@phine.tech>
2024-12-10 18:20:41 +0100

    chore(upf): clang-format

--- src/upf_app/bpf/rules/pdr/pfcp_session_lookup_xdp_kernel.c
+++ src/upf_app/bpf/rules/pdr/pfcp_session_lookup_xdp_kernel.c
@@ -110,7 +110,7 @@ handle_downlink_traffic(struct xdp_md* ctx, u32 ue_ip_address) {
     // check if it is a framed route address, and if yes try again the lookup
     // with the mapped address
     uint32_t big_endian_ue_ip = __builtin_bswap32(ue_ip_address);
-    #pragma clang loop unroll(full)
+#pragma clang loop unroll(full)
     for (uint32_t i = 32; i > 0; i--) {
       struct FramedRoutingKeyBPF key =
           framed_routing_key_for_ip_cidr(big_endian_ue_ip, i);
@@ -118,8 +118,7 @@ handle_downlink_traffic(struct xdp_md* ctx, u32 ue_ip_address) {
       uint32_t* fr_ue_ip =
           bpf_map_lookup_elem(&m_framed_route_mapping, &fr_key);
       if (fr_ue_ip) {
-        session =
-            bpf_map_lookup_elem(&m_session_mapping, fr_ue_ip);
+        session = bpf_map_lookup_elem(&m_session_mapping, fr_ue_ip);
         if (session) {
           u32 teid_dl = session->teid_dl;
           bpf_debug(
