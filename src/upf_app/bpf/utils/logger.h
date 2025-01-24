351147c35eca2bcafaed320b226b5e44c2bd51ea
Tariro Mukute <tariromukute@gmail.com>
2025-01-24 19:55:28 +0200

    Add enabling and disabling of BPF debug logging support and related build configuration

--- src/upf_app/bpf/utils/logger.h
+++ src/upf_app/bpf/utils/logger.h
@@ -6,8 +6,8 @@
 
 #include <bpf_helpers.h>
 
-#define DEBUG 1
-#ifdef DEBUG
+// #define DEBUG 0
+#ifdef BPF_DEBUG
 /* Only use this for debug output. Notice output from bpf_trace_printk()
  * end-up in /sys/kernel/debug/tracing/trace_pipe
  */
@@ -17,15 +17,7 @@
     bpf_trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__);                 \
   })
 #else
-#define bpf_debug(fmt, ...)                                                    \
-  {}                                                                           \
-  while (0)
+#define bpf_debug(fmt, ...)                                                    
 #endif
-#define bpf_debug2(fmt, ...)                                                   \
-  ({                                                                           \
-    char ____fmt[] = fmt;                                                      \
-    bpf_trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__);                 \
-  })
-#else
 
 #endif  // __LOGGER_H__
