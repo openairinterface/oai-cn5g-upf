ea0a719631cf09ae91f57cc024eb4f3b878caec6
Shubhika Garg <Shubhika.Garg@Openairinterface.org>
2026-03-25 15:05:57 +0100

    Remove EURECOM Copyright headers

--- src/oai_upf/main.cpp
+++ src/oai_upf/main.cpp
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2019 EURECOM
- *
  * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
