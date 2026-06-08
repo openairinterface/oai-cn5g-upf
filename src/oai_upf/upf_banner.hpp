9889c381ed462b522462d8a278fc51fa1a618f03
Franck Messaoudi <franck.messaoudi@eurecom.fr>
2026-06-08 17:49:36 +0200

    Fix: correct the license header
    Signed-off-by: Franck Messaoudi <franck.messaoudi@eurecom.fr>

--- src/oai_upf/upf_banner.hpp
+++ src/oai_upf/upf_banner.hpp
@@ -1,42 +1,5 @@
 /*
- * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The OpenAirInterface Software Alliance licenses this file to You under
- * the OAI Public License, Version 1.1  (the "License"); you may not use this
- * file except in compliance with the License.  You may obtain a copy of the
- * License at
- *
- *      http://www.openairinterface.org/?page_id=698
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *-------------------------------------------------------------------------------
- * For more information about the OpenAirInterface (OAI) Software Alliance:
- *      contact@openairinterface.org
- */
-
-/*!
- * \file   upf_banner.hpp
- * \author Franck MESSAOUDI
- * \date   2026
- * \email  franck.messaoudi@eurecom.fr
- * \see    3GPP TS 29.244 V17.10.0 (Release 17, 2024-04)
- *
- * \brief  Startup terminal banner for the OAI User Plane Function.
- *         v4 — uses ▓ (DARK SHADE U+2593) instead of █ (FULL BLOCK U+2588)
- *              to force pixel-art look with visible gaps in every
- * font/terminal. OAI ▓ = bright blue, OAI ╗╔║╚╝═ = bright white. UPF ▓ = random
- * green/white mix.
- *
- * Usage:
- *   #include "upf_banner.hpp"
- *   oai::upf::print_banner();
- *
- * Color output is guarded by isatty(STDOUT_FILENO).
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
  */
 
 #pragma once
